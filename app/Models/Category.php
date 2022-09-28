2dd4a5e85b8823ffb52eb4a8d766e21d104420c4
dayanaYl <111261588+dayanaYl@users.noreply.github.com>
2022-09-27 23:05:20 -0500

    Actores y categorias

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -7,11 +7,18 @@ use Illuminate\Database\Eloquent\Model;
 
 class Category extends Model
 {
+      //La tabla a conectar a este modelo
+      protected $table="category";
+
+      //La clave primaria de esa tabla 
+      protected $primaryKey = "category_id";
+      //omitir campos de auditoria 
+      public $timestamps =false; 
+
     use HasFactory;
         
     //fillable: permite realizar 
     //insertar varias instaias al tiempo
-    protected $fillable=['name', 
-                        'last_update'
+    protected $fillable=['name'
                     ]; 
 }
